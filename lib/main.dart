42cf9b2127913aae9154fa0854817736cb9d80ae
Ryan Harper <ryan.harper.rh@gmail.com>
2020-06-07 21:01:31 -0500

    Added card padding and flatbutton widget.

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,43 @@ import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
-      MaterialApp(
-        home: Scaffold(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Cards'),
           backgroundColor: Colors.red,
-          appBar: AppBar(
-            title: Text('Cards'),
-            backgroundColor: Colors.red,
-          ),
-          body: CardsPage(),
         ),
+        body: CardsPage(),
       ),
+    ),
   );
 }
 
 class CardsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                print('left card pressed.');
+              },
+              child: Image.asset('images/2H.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                print('right card pressed.');
+              },
+              child: Image.asset('images/2S.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
-
